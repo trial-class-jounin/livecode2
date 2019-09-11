71b58250e0ce61688c5e6443cf6f2b4015221b2e
BlueHeavenID <54161201+BlueHeavenID@users.noreply.github.com>
2019-09-11 09:40:21 +0900

    updated with handmade code, if/for only

--- petra-livecoding-ketiga-no1.js
+++ petra-livecoding-ketiga-no1.js
@@ -1,48 +1,35 @@
 function arrayRemover (array, index, count) {
 
-//tes pertama
 
-//   for (var i = 0 ; i < count ; i++){
-    
-//     if (index === 0 ) {
-//       array[i] = ''
-//     } else {
-//       array[i+1] = ''
-//     }
-  
-//   }
+//asli bikinan sendiri - petra
+
+var systemCallGenerateResult = "["
+//is that SAO Alicization reference?
+//i am not weebs for nothing, ya
+for(var i=0;i<array.length;i++){
+  //cari mulai kapan remove array
+ if(i == index || i> index && i <= index+ count-1){ 
+    //kalo dah nemu array dari mana sampe mana yang mau diremove
+    //skip, literally do nothing
+    }
+  else{
+    //selain dari array yang diremove, semua nambahin ke var systemCallGenerateResult, gampangnya istilah array itu push, tapi disini kita pake if else sama for doang, sangat tradisional
+    if(systemCallGenerateResult == "["){
+    systemCallGenerateResult += array[i]
+    array[i+1]  
+    }
+    else{
+    systemCallGenerateResult += ", " + array[i]
+    array[i+1]
+    }
+  }
+}
+systemCallGenerateResult += "]"
+return systemCallGenerateResult
+
+//cara gampang
+// array.splice(index,count)
 // return array
-
-//test kedua
-
-// var systemCallGenerateResult = "["
-// for(var i=0;i<array.length;i++){
-//   if(i == index){
-//     var systemCallGenerateCount = i+count
-//     array[systemCallGenerateCount]
-//     var systemCallGenerateLast= array.length-systemCallGenerateCount
-//     for(var j=0;j<systemCallGenerateLast;j++){
-//     systemCallGenerateResult += ", " + array[i]
-//     array[i+1]
-//     }
-//   }
-//   else{
-//     if(i=0){
-//     systemCallGenerateResult += array[i]
-//     array[i+1]  
-//     }
-//     else{
-//     systemCallGenerateResult += ", " + array[i]
-//     array[i+1]
-//     }
-//   }
-// }
-// systemCallGenerateResult += "]"
-// return systemCallGenerateResult
-
-
-array.splice(index,count)
-return array
 }
 
 console.log(arrayRemover([ 1, 2, 3] , 0 , 2 )); // [3]
